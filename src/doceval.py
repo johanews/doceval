c81566096e22ec5c5b8e4efe67d18c161abc7bad
Johan Sjöberg <johasjob@student.chalmers.se>
2018-10-29 10:13:11 +0100

    Take regex as input enabling block specific searches and use concurrecy

--- src/doceval.py
+++ src/doceval.py
@@ -1,5 +1,6 @@
 import os
 import re
+import multiprocessing as mp
 
 
 def scan_dir(dir):
@@ -20,25 +21,53 @@ def scan_dir(dir):
     return files
 
 
-def evaluate(files):
+def cls_eval(files):
     """
     Scan each file from the input list, searching for
-    undocumented methods using regex matching.
+    undocumented classes.
 
     :param files: the list of files
-    :return: a dictionary of the methods in each file
-    that are not documented
+    :return: a dict with all the undocumented classes
+    in each file
+    """
+    regex = r'class\s(\w+\([\w.]*\)):'
+    evaluate(files, regex)
+
+
+def fun_eval(files):
+    """
+    Scan each file from the input list, searching for
+    undocumented methods.
+
+    :param files: the list of files
+    :return: a dict with all the undocumented methods
+    in each file
+    """
+    regex = r'def\s(\w+\(\w*\)):'
+    evaluate(files, regex)
+
+
+def evaluate(files, regex):
+    """
+    Scan each file from the input list, searching for
+    undocumented code-blocks of the type specified by
+    the regex argument.
+
+
+    :param files: the list of files
+    :param regex: the regex
+    :return: a dict containing all the code-blocks in
+    each file that are not documented
     """
     check_doc = False
     preceding = ()
-    undoc_fun = {}
-    fun_count = 0
+    undoc_blk = {}
+    blk_count = 0
 
-    fun_regex = r'def\s(\w+\(\w*\)):'
     doc_regex = r'^\s*"""\n'
 
     for file in files:
-        undoc_fun[file] = {}
+        undoc_blk[file] = {}
 
         for i, line in enumerate(open(file)):
             if bool(line.strip()):
@@ -46,25 +75,34 @@ def evaluate(files):
                     match = re.search(doc_regex, line)
                     if not match:
                         fun = {preceding[0]: preceding[1]}
-                        undoc_fun[file].update(fun)
-                match = re.search(fun_regex, line)
+                        undoc_blk[file].update(fun)
+                match = re.search(regex, line)
                 if match:
-                    fun_count = fun_count + 1
+                    blk_count = blk_count + 1
                     preceding = (match.group(1), i + 1)
                     check_doc = True
                 else:
                     check_doc = False
 
-        if not bool(undoc_fun[file]):
-            del undoc_fun[file]
+        if not bool(undoc_blk[file]):
+            del undoc_blk[file]
 
-    print(undoc_fun)
+    print(undoc_blk)
 
 
 def main():
     path = os.getcwd()
     assert os.path.isdir(path)
-    evaluate(scan_dir(path))
+    files = scan_dir(path)
+
+    p1 = mp.Process(target=cls_eval, args=(files,))
+    p2 = mp.Process(target=fun_eval, args=(files,))
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
 
 
 if __name__ == "__main__":
